8509a2f79c0da6f418f67e147d60f00d01fce95d
TheMario <themario80@gmail.com>
2017-09-13 22:42:59 -0500

    adding a new function

--- WikiaReader/WikiaReader.py
+++ WikiaReader/WikiaReader.py
@@ -1,3 +1,4 @@
+from parse import *
 import discord
 import wikia
 from discord.ext import commands
@@ -10,25 +11,26 @@ class WikiaReader:
         self.__log = open("test.txt", 'w')
         self.__query = list()
         
-    ''' 
-    def __say(self, message):
-       message = str(message)
-       await self.bot.say(message)
-       #print()
-       self.__log.write(message + "\n")'''
-        
-    @commands.command()   
-    async def WikiQuery(self, message):
-        '''This will help you do a query Search of the wiki'''
+    def __query(self, term):
         query = wikia.search(self.__wiki, message, results=50)
-        #log = open("test.txt", 'w')
         queryResults = "We found " + str(len(query)) + " results. Here are the top 10 results!\n"
         await self.bot.say(queryResults)
         
+        queryMessage = "";
+        
         for i in range(0,10):
             i = i + 1
-            await self.bot.say(str(i) + " " +query[i])
-    
+            queryMessage += str(i) + " " +query[i] + "\n";
+            
+        await self.bot.say(queryMessage)
+        await self.bot.say("Please select which one you'll like to choose using [p]Wikia select [#]")
+        
+    @commands.command()   
+    async def Wikia(self, message):
+        '''Base Command for everything Wikia!'''
+        cmd,input = parse("{0} {1}", message)
+        await self.bot.say("CMD: " + cmd + " input: " +  input)
+        
     
     
 def setup(bot):
